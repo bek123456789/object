64c20c41ba5a8e79c0cd407784e9c0092b290964
bek123456789 <redmirating@gmail.com>
2024-04-26 21:54:43 +0500

    done

---
+++ main.js
@@ -0,0 +1,13 @@
+let promFirst = prompt(`Ismingizini kirting!`)
+let promSecond = prompt('Space idingizni yozing!')
+let promThird = prompt("Space passwordingizni yozing!")
+
+
+let arr = {
+    id: promSecond,
+    name: promFirst,
+    password: promThird
+};
+
+
+console.log(arr);
